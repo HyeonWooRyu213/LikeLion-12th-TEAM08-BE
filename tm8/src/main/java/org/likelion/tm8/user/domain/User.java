e66e579af5ef0989c3fad523f382926597d916f9
HyeonWooRyu213 <fbgusdn05@gmail.com>
2024-08-01 16:19:20 +0900

    관계설정 완료

--- tm8/src/main/java/org/likelion/tm8/user/domain/User.java
+++ tm8/src/main/java/org/likelion/tm8/user/domain/User.java
@@ -1,4 +1,31 @@
 package org.likelion.tm8.user.domain;
 
+import jakarta.persistence.*;
+import org.likelion.tm8.diet.domain.Diet;
+import org.likelion.tm8.exercise.domain.Exercise;
+import org.likelion.tm8.today.domain.Today;
+import org.likelion.tm8.weight.domain.Weight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Diet> dietList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Exercise> exerciseList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Today> todayList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Weight> weightList = new ArrayList<>();
 }
